d3c963137b046e6ab96b0269c3b8087623f0aeec
Ziana Aulia <zianaaulia2602@gmail.com>
2022-04-21 13:54:57 +0700

    Implementasi Fragment Statis di Kotlin

---
+++ app/src/main/java/zianaaulia/polbeng/ac/id/fragmentexample1/Coordinator.kt
@@ -0,0 +1,5 @@
+package zianaaulia.polbeng.ac.id.fragmentexample1
+
+interface Coordinator {
+    fun onBookChanged(index:Int)
+}
--- app/src/main/java/zianaaulia/polbeng/ac/id/fragmentexample1/MainActivity.kt
+++ app/src/main/java/zianaaulia/polbeng/ac/id/fragmentexample1/MainActivity.kt
@@ -2,10 +2,17 @@ package zianaaulia.polbeng.ac.id.fragmentexample1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Coordinator {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+    override fun onBookChanged(index:Int) {
+        val frag = fragmentbookdescription
+        if (frag is BookDescription){
+            frag.changeDescription(index)
+        }
     }
 }
